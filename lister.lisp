ad6b2fd34490648aaf3c4f06b1b3be7338ec41a5
Drewry Pope <dezren39@users.noreply.github.com>
2017-02-19 03:04:57 -0600

    Update lister.lisp

--- lister.lisp
+++ lister.lisp
@@ -1,18 +1,26 @@
-lists single items until item is below 0
 
+
+;;Prints a list one line at a time 
 (defun lister (list) (loop
-      (when (>= 0 (car list)) (return "Done"))
+     (when (>= 0 (car list)) (return "Done"))
+     (print (car list)) (setf list (cdr list))))
+;Example with algorithm from stats.      
+(lister (loop for x from 20 to 100 for y = (- 97 (* 1.45 x)) collect y))
+
+;Prints multi-item lists until first item in first list is below equal 0
+(defun lister1 (list) (loop
+      (when (>= 0 (car (car list))) (return "Done"))
       (print (car list)) (setf list (cdr list))))
-      
-      (lister (loop for x from 20 to 100 for y = (- 97 (* 1.45 x)) collect y))
-      
-      
-      lists 2 item lists until 1 item below equal 0
-      (defun lister1 (list) (loop (when (>= 0 (car (car list))) (return "Done"))(print (car list)) (setf list (cdr list))))
-      
-      
-      same thing but scond item? hard time this way
-      (defun lister1 (list) (loop (when (> 0 (car (cdr (car list)))) (return "Done"))(print (car list)) (setf list (cdr list))))
-      
-      loops and collects a list with the x and y
-      (lister1 (loop for x from 20 to 100 for y = (list x (- 97 (* 1.45 x))) collect y))))
+
+;Same thing but second item? Had a hard time this way, thought I'd find a better way than car/cdr/car, but eh.
+(defun lister2 (list) (loop (when (> 0 (car (cdr (car list)))) (return "Done"))(print (car list)) (setf list (cdr list))))
+
+;Example prints lists with the x and y for each x from 20 to 100. (same stats algo, age/insuranceCost)
+(lister2 (loop for x from 20 to 100 for y = (list x (- 97 (* 1.45 x))) collect y))))
+
+;CLISP prints this error:
+;     *** - READ from #<INPUT CONCATENATED-STREAM #<INPUT STRING-INPUT-STREAM> #<IO TERMINAL-STREAM>>: an object cannot
+;     start with #\)
+;     The following restarts are available:
+;     ABORT          :R1      Abort main loop
+;Even though it's successful. Look into later.
