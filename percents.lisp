298dd5cc2315f899defc54d0916cfec5fc4c9720
Drewry Pope <dezren39@users.noreply.github.com>
2017-03-01 21:17:38 -0600

    Update percents.lisp

--- percents.lisp
+++ percents.lisp
@@ -1,2 +1,3 @@
 (defun percents (a b) (list (+ a b 0.0) (/ a (+ a b 0.0)) (/ b (+ a b 0.0)) ) )
 (defun % (a b) (list (list "a : " (* 100 (/ a (+ a b 0.0))) "b : " (* 100 (/ b (+ a b 0.0)))) (list a " + " b "=" (+ a b))))
+(defun p (a b) (list "=" a "/" b "=" (+ 0.0 (/ a b))))
